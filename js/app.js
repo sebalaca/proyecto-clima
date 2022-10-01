244cfccd73454863d87ffb78dc080dec81e34e90
Sebastian <seba.laca@gmail.com>
2022-10-01 17:42:59 +0200

    Update app

--- js/app.js
+++ js/app.js
@@ -55,8 +55,40 @@ function consultarApi(ciudad, pais) {
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
+            limpiarHTML()
+            console.log(datos);
             if (datos.cod === '404') {
                 mostrarError('Ciudad no encontrada')
+                return
             }
+
+            //Imprime la respuesta en el HTMl
+            mostrarClima(datos)
+
         })
+}
+
+function mostrarClima(datos) {
+    const {main: {temp, temp_max, temp_min}} = datos
+    const centigrados = kelvinACentigrados(temp)
+
+    const actual = document.createElement('p')
+    actual.innerHTML = `${centigrados} &#8451;`
+    actual.classList.add('font-bold', 'text-6xl')
+
+    const resultadoDiv = document.createElement('div')
+    resultadoDiv.classList.add('text-center', 'text-white')
+    resultadoDiv.appendChild(actual)
+
+    resultado.appendChild(resultadoDiv)
+}
+
+function kelvinACentigrados(grados) {
+    return parseInt(grados - 273.15)
+}
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild)
+    }
 }
