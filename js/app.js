f2cde0637cb820393fa406a83dbbe7725756b9d8
Sebastian <seba.laca@gmail.com>
2022-10-01 13:51:57 +0200

    Update app

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('.container')
+const resultado = document.querySelector('#resultado')
+const formulario = document.querySelector('#formulario')
+
+//similar a DOMContentLoader
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima)
+})
+
+
+function buscarClima(e) {
+    e.preventDefault()
+
+    //validar formulario
+    const ciudad = document.querySelector('#ciudad').value
+    const pais = document.querySelector('#pais').value
+
+    if (ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios')
+        return
+    }
+
+    //consultar API
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100')
+
+    if(!alerta){
+        //Creamos alerta con scripting
+        const alerta = document.createElement('div')
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center')
+        alerta.innerHTML = ` 
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `
+    
+        container.appendChild(alerta)
+
+        //Eliminar alerta despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove()
+        }, 5000);
+    }
+}
